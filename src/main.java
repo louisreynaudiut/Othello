4d35c5444796137213b98aa4916b6392e6d40c31
reynloui <reynloui@PC-B103-04.iut-valence.fr>
2016-02-19 14:14:26 +0100

    Declaration of the main class.

---
+++ src/main.java
@@ -0,0 +1,13 @@
+/**
+ * the main class represent a game of Othello
+ * @author reynloui/palmierb
+ */
+public class main {
+
+	public static void main(String[] args) {
+		
+		OthelloGame playOthello = new othelloGame();
+		playOthello.start();
+	}
+
+}
