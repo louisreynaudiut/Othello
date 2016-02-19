efb51254796f30084c466d382afd13e727787002
reynloui <reynloui@PC-B103-04.iut-valence.fr>
2016-02-19 14:31:20 +0100

    Creation of the OthelloGame class.

---
+++ src/OthelloGame.java
@@ -0,0 +1,12 @@
+/**
+ * Creates a new game of Othello ready to be played.
+ * @author reynloui
+ *
+ */
+public class OthelloGame 
+{
+	public void start()
+	{
+		system.out.println("\n La partie est prête à être lancée.");
+	}
+}
